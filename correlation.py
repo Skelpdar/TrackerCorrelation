057e78bd92ed9b044142dc354b700ea4ac81b960
Erik Wallin <skelpdar@gmail.com>
2022-04-20 21:06:06 +0200

    Various changes

--- correlation.py
+++ correlation.py
@@ -15,6 +15,7 @@ p = {
     #"TSFPGAFrequency":1.248623529*0.9995150078*0.9999702524*1.000012886*0.9999961275*0.9999993372/0.35528982*0.35528970*0.429724/0.42971903*0.428688011/0.428687812*0.444850191/0.444849690*1e-6/156., #Frequency of TS FPGA
     "TSFPGAFrequency":8.000046470962321e-09, #Frequency of TS FPGA
     "TimestampTolerance": 5e-7, #Difference (in seconds) for where two timestamps will be considered equal
+    "HCalTimestampTolerance": 5e-6, #Difference (in seconds) for where two timestamps will be considered equal
     "FTToWRDistance": 0.0021-2.872e-6 #Time difference between telescope trigger signals in WR and FT
 }
 
@@ -31,80 +32,74 @@ class TSSpill:
     def toIAT(self, WRSpills):
 
         spillCandidates = []
-        for i in range(0,len(WRSpills)):
-            if abs(WRSpills[i].startOfSpill-self.coarse) < 50:
-                spillCandidates.append(i)
+        #for i in range(0,len(WRSpills)):
+        #    if abs(WRSpills[i].startOfSpill-self.coarse) < 50:
+        #        spillCandidates.append(i)
+
+        spillCandidates = list(range(0,len(WRSpills)))
+        print(spillCandidates)
 
         for i in spillCandidates:
+            print(i)
             WRSpill = WRSpills[i]
 
-            for skip in [0,1,2,3,4]:
-                    #bins = np.linspace(0,0.5,5000000)
+            for skip in [0,1,2,3,4,5,6]:
+                    try:
+                        bins = np.linspace(0,0.5,5000000)
 
-                    #Keep track of how we have translated the WR timestamps
-                    deltat = 0
+                        #Keep track of how we have translated the WR timestamps
+                        deltat = 0
 
-                    tsnew = np.array(self.ticks)
+                        tsnew = np.array(self.ticks)
 
-                    tsnew = np.array(tsnew - min(tsnew))
-                    deltat += min(WRSpill.timestamps)
-                    timestampsnew = np.array(WRSpill.timestamps) - min(WRSpill.timestamps)
+                        tsnew = np.array(tsnew - min(tsnew))
+                        deltat += min(WRSpill.timestamps)
+                        timestampsnew = np.array(WRSpill.timestamps) - min(WRSpill.timestamps)
 
-                    #Try skipping initial few WR timestamps
-                    timestampsnew = timestampsnew[skip:]
-                    
-                    tsnew = (np.array(tsnew) - min(tsnew))
-                    deltat += min(timestampsnew)
-                    timestampsnew = np.array(timestampsnew) - min(timestampsnew)
+                        #Try skipping initial few WR timestamps
+                        timestampsnew = timestampsnew[skip:]
+                        
+                        tsnew = (np.array(tsnew) - min(tsnew))
+                        deltat += min(timestampsnew)
+                        timestampsnew = np.array(timestampsnew) - min(timestampsnew)
 
-                    #tsnew = tsnew*1.248623529*0.9995150078*0.9999702524*1.000012886*0.9999961275*0.9999993372/0.35528982*0.35528970
-                    tsnew = tsnew*p["TSFPGAFrequency"]
+                        tsnew = tsnew*p["TSFPGAFrequency"]
 
-                    #if(True):
-                    #    bins = np.linspace(0.4,0.45,500000)
+                        #plt.hist(timestampsnew, bins=bins, histtype="step", linewidth=2)
+                        #plt.hist(tsnew, bins=bins, histtype="step")
+                        #plt.show()
+                        
+                        #Check whether we have a match
+                        matches = 0
+                        misses = 0
+                        for k in tsnew:
+                            diff = np.sort(np.abs(timestampsnew-k))
+                            if diff[1] < p["TimestampTolerance"]:
+                                misses += 1
+                            elif diff[0] < p["TimestampTolerance"]:
+                                matches += 1
+                                d = np.abs(timestampsnew-k)
+                            else:
+                                misses += 1
 
-                    #    plt.hist(tsnew,histtype="step",bins=bins,density=True)
-                    #    plt.hist(timestampsnew,bins=bins,histtype="step", density=True)
-                    #    plt.show()
+                        print("Efficiency: " + str(matches/len(tsnew)))
 
-                    #Check whether we have a match
-                    #closesttimestamp = []
-                    matches = 0
-                    misses = 0
-                    for k in tsnew:
-                        diff = np.sort(np.abs(timestampsnew-k))
-                        if diff[1] < p["TimestampTolerance"]:
-                            misses += 1
-                            #closesttimestamp.append(-1)
-                        elif diff[0] < p["TimestampTolerance"]:
-                            matches += 1
-                            d = np.abs(timestampsnew-k)
-                            #closesttimestamp.append(float(timestampsnew[np.where(d == d.min())])+deltat+startofspill[i])
-                        else:
-                            misses += 1
-                            #closesttimestamp.append(-1)
+                        if matches/len(tsnew) < 0.2:
+                            continue
+                        matched = True
 
-                    print("Efficiency: " + str(matches/len(tsnew)))
+                        return tsnew + deltat, i
 
-                    if matches/len(tsnew) < 0.5:
-                        continue
-                    matched = True
-                    #results.append(matches/len(tsnew))
-                    #print("Matched")
-
-                    #print(tsnew)
-                    #print(deltat)
-
-                    return tsnew + deltat, i
-
-                    break
+                        break
+                    except:
+                        print("Exception?")
         else:
             raise Exception("Could not correlate TS spill with WR")
 
 """
 Takes a plaintext TS datafile and extracts all clock tick counts in event headers
 
-I do not trust to never miss events. 
+I do not trust this to never miss events. 
 """
 def readTS(filename):
     file = open(filename)
@@ -127,7 +122,11 @@ def readTS(filename):
                 value = str(bin(int(lines[i+1][8:],16))[2::].zfill(32))[::-1]
                 #Extract to clock ticks in the event header
                 #v = int(value[0:8][::-1] + value[8:16][::-1] + value[16:24][::-1]+ value[24:32][::-1],2)
-                v = int(value[0:8][::-1][6:] + value[8:16][::-1] + value[16:24][::-1]+ value[24:32][::-1],2)
+                v = int(value[0:8][::-1] + value[8:16][::-1] + value[16:24][::-1]+ value[24:32][::-1],2)
+
+                if v == 0:
+                    value = str(bin(int(lines[i+2][8:],16))[2::].zfill(32))[::-1]
+                    v = int(value[0:8][::-1] + value[8:16][::-1] + value[16:24][::-1]+ value[24:32][::-1],2) 
 
                 if len(spills[-1].ticks) == 0:
                     spills[-1].ticks.append(v)
@@ -200,7 +199,7 @@ class FT:
     """
     Take a list of IAT timestamps
     """
-    def separateTSEvents(self, tstimestamps, tsspill, wrStartOfSpill):
+    def separateTSEvents(self, tstimestamps, tsspill, wrStartOfSpill, outputfile):
         ftnew = self.timestamps.copy()
 
         ftnew = np.array(ftnew) - wrStartOfSpill
@@ -231,10 +230,12 @@ class FT:
                 matches += 1
                 index = diff.index(sort[0])
                 print(str(tsspill.ticks[i])+":"+("".join([str(s)+"," for s in self.data[index]])[:-1]))
+                outputfile.write(str(tsspill.ticks[i])+":"+("".join([str(s)+"," for s in self.data[index]])[:-1])+"\n")
                 pass
             if hit == False:
                 #print(tsspill.ticks[i])
                 print(str(tsspill.ticks[i])+":"+"")
+                outputfile.write(str(tsspill.ticks[i])+":"+"\n")
 
         #print("FTV efficiency: " + str(matches/len(TSTimestamps)))
         
@@ -262,37 +263,136 @@ def readFT(filename):
 
     return ft
 
+class HCalSpill:
+    def __init__(self):
+        self.timestamps = []
+
+    def toIAT(self, WRSpills):
+        spillCandidates = [0,1,2]
+        #for i in range(0,len(WRSpills)):
+        #    if abs(WRSpills[i].startOfSpill-self.coarse) < 50:
+        #        spillCandidates.append(i)
+
+        for i in spillCandidates:
+            WRSpill = WRSpills[i]
+
+            for skip in [0,1,2,3,4,5,6,7]:
+                    bins = np.linspace(0,5,5000000)
+
+                    #Keep track of how we have translated the WR timestamps
+                    deltat = 0
+
+                    tsnew = np.array(self.timestamps)
+
+                    tsnew = np.array(tsnew - min(tsnew))
+                    deltat += min(WRSpill.timestamps)
+                    timestampsnew = np.array(WRSpill.timestamps) - min(WRSpill.timestamps)
+
+                    #Try skipping initial few WR timestamps
+                    timestampsnew = timestampsnew[skip:]
+                    
+                    tsnew = (np.array(tsnew) - min(tsnew))
+                    deltat += min(timestampsnew)
+                    timestampsnew = np.array(timestampsnew) - min(timestampsnew)
+
+                    #tsnew = tsnew*p["TSFPGAFrequency"]
+                   
+                    plt.hist(tsnew, bins=bins, histtype="step",linewidth=2) 
+                    plt.hist(timestampsnew, bins=bins, histtype="step") 
+                    plt.show()
+     
+                    #Check whether we have a match
+                    matches = 0
+                    misses = 0
+                    for k in tsnew:
+                        diff = np.sort(np.abs(timestampsnew-k))
+                        if diff[1] < p["HCalTimestampTolerance"]:
+                            misses += 1
+                        elif diff[0] < p["HCalTimestampTolerance"]:
+                            matches += 1
+                            d = np.abs(timestampsnew-k)
+                        else:
+                            misses += 1
+
+                    print("Efficiency: " + str(matches/len(tsnew)))
+
+                    if matches/len(tsnew) < 0.2:
+                        continue
+                    matched = True
+
+                    return tsnew + deltat, i
+
+                    break
+        else:
+            raise Exception("Could not correlate TS spill with WR")
+
+
+def readHCal(filename):
+    file = open(filename)
+
+    spills = []
+
+    oldtimestamp = 999999999
+    for l in file:
+        timestamp = float(l)/(5e6)
+        if timestamp < oldtimestamp:
+            spills.append(HCalSpill())
+            spills[-1].timestamps.append(timestamp)
+            oldtimestamp = timestamp
+        else:
+            spills[-1].timestamps.append(timestamp)
+            oldtimestamp = timestamp
+    
+    return spills
+
+
 if __name__ == '__main__':
     outputfilename = ""
     TSfilename = ""
     WRfilename = ""
-    FTfilename = ""
+    FTdvfilename = ""
 
     if len(sys.argv) == 2:
         print("Using default data paths")
         outputfilename = sys.argv[1]
-        TSfilename = "data/positive4GevElectrons_Mar28_1455.txt"
-        WRfilename = "data/March28_calibration_1.txt"
-        FTfilename = "data/March28_1150_calibrationrun_1_51.txt"
-    elif len(sus.argv) == 5:
+        TSfilename = "data/ldmx_captan_out_17-04-2022_21-14-11__172.txt"
+        WRfilename = "data/WR_out_172.txt"
+        FTdvfilename = "data/DipClient_out_run_172_51.txt"
+    elif len(sys.argv) == 5:
         outputfilename = sys.argv[1]
         TSfilename = sys.argv[2]
         WRfilename = sys.argv[3]
-        FTfilename = sys.argv[4]
+        FTdvfilename = sys.argv[4]
     else:
+        print("Incorrect arguments")
         quit()
 
     print("Reading TS data")
     TSSpills = readTS(TSfilename)
 
+    print(len(TSSpills))
+
     print("Reading WR data")
     WRSpills = readWR(WRfilename)
+    print(len(WRSpills))
+
+    #print("Reading HCal data")
+    #HCalSpills = readHCal("data/hcal_172_timestamps.txt")
+
+    #for i in range(0,len(HCalSpills)):
+    #    iat = HCalSpills[i].toIAT([WRSpills[i]])
 
     print("Reading FT data")
     #Fiber tracker 51 is the downstream vertical tracker, 50 is the downstream horizontal
-    FT_DownstreamVertical = readFT(FTfilename)
+    #42 is upstream vertical, and 41 is upstream horizontal
+    FT_DownstreamVertical = readFT(FTdvfilename)
+    
+    outputfile = open(outputfilename, "w")
+   
+    print(len(TSSpills[1].ticks))
 
-    #print(TSSpills[1].ticks)
+    WRSpills = WRSpills[16:]    
+
      
     print("Correlating TS and WR and FT data")
     for i in range(0,len(TSSpills)):
@@ -302,10 +402,10 @@ if __name__ == '__main__':
 
             print("Correlating hits in fiber tracker with TS events")
             #This prints out a line for each TS event, with a comma separated list of hit positions in the fibers
-            FT_DownstreamVertical.separateTSEvents(TSTimestamps, TSSpills[i], WRSpills[spillnumber].startOfSpill)
+            FT_DownstreamVertical.separateTSEvents(TSTimestamps, TSSpills[i], WRSpills[spillnumber].startOfSpill, outputfile)
         except:
             print("Could not correlate " + str(i))
             print("Zeros for all TS events")
             for k in TSSpills[i].ticks:
-                print(str(k)+":")
+                outputfile.write(str(k)+":"+"\n")
     
